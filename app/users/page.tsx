4eeb97ff4ee2b7185120ec335e26b4a0dee98c5a
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-08 19:03:57 +0900

    UserListコンポーネントの作成

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,23 +1,13 @@
-type User = {
-  id: string;
-  name: string;
-  email: string;
-};
+import UserList from './UserList';
 
 const Page = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    // usersページへのリンクをクリックします。クリックして5秒間何も画面に変化はありません。5秒経過するとユーザ一覧が表示されます。このことからServer Componentでの処理が完了するまでページが表示されないことがわかりました。
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users: User[] = await response.json();
-  console.log(users);
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">ユーザ一覧</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      {/* @ts-expect-error Async Server Component */}
+      <UserList />
+      {/* page.tsxファイルのUserListタグの箇所にTypeScriptに関するメッセージがに表示されます。
+      エラーの表示される原因はUserListコンポーネントがasyncを利用した非同期のServer Componentのためです。asyncを利用していないコンポーネントの場合にメッセージは表示されません。 */}
     </div>
   );
 };
