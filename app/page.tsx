a34bb30f7d4a5db081a42040c0222ad60b066c0d
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-09 11:19:09 +0900

    ClientComponentなCounterコンポーネントの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
-import Link from 'next/link';
+// 'use client' 親にuse clientを適用させると、子供のコンポーネントは全てClient Componentになる
+import Link from "next/link";
+import { Counter } from "./counter";
 
 export default function Home() {
   return (
     <div className="m-4">
-      {/* prefetch : Linkコンポーネントではデフォルトからprefetchの機能が設定されています。開発環境ではリンクにカーソルを当てるとリンク先のページに関するJavaScriptファイルなどがバックグランドで自動でダウンロードされます。本番環境ではViewportに入っているリンク先のファイルがバックグランドで自動でダウンロードされます。
-      
-      prefect機能を利用したくないという場合はLinkコンポーネントのprefetch propsをfalseにすることで無効化できます。
-      */}
       <Link href="/about" className="underline" prefetch={false}>
         About
       </Link>
       <h1 className="text-2xl">Home</h1>
+      <Counter />
     </div>
   );
 }
