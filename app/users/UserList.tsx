b64b9c741e3ac215d0fb7c6f46d8a3424aecc334
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-10 16:13:03 +0900

    POSTの設定

--- app/users/UserList.tsx
+++ app/users/UserList.tsx
@@ -1,25 +1,27 @@
-import { NextResponse } from 'next/server';
+import UserList from './UserList';
 
-type User = {
-  id: string;
-  name: string;
-  email: string;
-};
-const UserList = async () => {
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch('http://localhost:3000/api?name=John', {
-  cache: 'no-store', 
-});
-  if (!response.ok) throw new Error('Failed to fetch data');
-  const users: User[] = await response.json();
+const Page = async () => {
+  const response = await fetch('http://localhost:3000/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'John',
+      email: 'john@example.com',
+    }),
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <div className="m-4">
+      <h1 className="text-lg font-bold">ユーザ一覧</h1>
+      <UserList />
+    </div>
   );
 };
 
-
-export default UserList;
+export default Page;
