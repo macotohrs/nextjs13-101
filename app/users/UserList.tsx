3e73031b48e163b24f84c40851bc70c8d363f5a0
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-10 16:38:22 +0900

    users/page, UserListの記述を修正

--- app/users/UserList.tsx
+++ app/users/UserList.tsx
@@ -1,27 +1,35 @@
-import UserList from './UserList';
-
-const Page = async () => {
-  const response = await fetch('http://localhost:3000/api', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name: 'John',
-      email: 'john@example.com',
-    }),
-  });
-
-  const data = await response.json();
-
-  console.log(data);
-
-  return (
-    <div className="m-4">
-      <h1 className="text-lg font-bold">ユーザ一覧</h1>
-      <UserList />
-    </div>
-  );
+type User = {
+  id: string;
+  name: string;
+  email: string;
 };
 
-export default Page;
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name');
+  console.log(name);
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  return NextResponse.json(data);
+}
+
+
+const UserList = async () => {
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  // 検索などURLパラメータを利用した場合のURLパラメータの取得方法
+  const response = await fetch('http://localhost:3000/api?name=John', {
+  cache: 'no-store', // キャッシュ機能を無効 (一度fetch関数が実行されるとキャッシュされるためその後fetch関数を実行するとキャッシュしたデータが利用されるためリクエストが行われない)
+});
+  if (!response.ok) throw new Error('Failed to fetch data');
+  const users: User[] = await response.json();
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+};
+export default UserList;
